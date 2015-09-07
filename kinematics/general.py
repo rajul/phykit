cc82cbdc30d59412c308e8ac97e95f5be34637d4
Rajul Srivastava <rajul09@gmail.com>
2015-09-07 20:54:42 +0530

    Added more skeleton code to measurement and kinematics

---
+++ kinematics/general.py
@@ -0,0 +1,2 @@
+class General(object):
+	pass
--- measurement/quantity.py
+++ measurement/quantity.py
@@ -2,3 +2,6 @@ class Quantity(object):
 	def __init__(self, magnitude, unit):
 		self.magnitude = magnitude
 		self.unit = unit
+
+	def is_dimensionally_consistent(self, other):
+		pass
